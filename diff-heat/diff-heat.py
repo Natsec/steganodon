89a70ee65eaee1078dfbf132d48bdb074b6e6110
Natsec <kamilp@hotmail.fr>
2020-10-11 12:29:43 +0200

    affichage

--- diff-heat/diff-heat.py
+++ diff-heat/diff-heat.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+"""
+TODO: trois images dans une fenêtre
+
+"""
+
+image1 = img.imread(sys.argv[1])
+# image2 = img.imread(sys.argv[2])
+
+channel = ["Red", "Green", "Blue"]
+for i in range(3):
+	plt.figure("Channel {} : {}".format(i, channel[i]))
+	plt.imshow(image1[:, :, i])
+	plt.colorbar()
 
 
-plt.imshow(image1)
-plt.colorbar()
 plt.show()
