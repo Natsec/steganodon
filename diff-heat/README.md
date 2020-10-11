9179936cd46730f293c20d08345b6d6840be4f00
Natsec <kamilp@hotmail.fr>
2020-10-11 20:15:29 +0200

    plusieurs images

---
+++ diff-heat/README.md
@@ -0,0 +1,7 @@
+# Diff-Heat
+Visualizing differences between two RGB images.
+
+## Usage
+```
+./diff-heat.py image1.jpg image2.jpg
+```
--- diff-heat/diff-heat.py
+++ diff-heat/diff-heat.py
@@ -13,11 +13,32 @@ TODO: trois images dans une fenêtre
 image1 = img.imread(sys.argv[1])
 # image2 = img.imread(sys.argv[2])
 
-channel = ["Red", "Green", "Blue"]
-for i in range(3):
-	plt.figure("Channel {} : {}".format(i, channel[i]))
-	plt.imshow(image1[:, :, i])
-	plt.colorbar()
+channel = ["Red", "Green", "Blue", ""]
+# for i in range(3):
+# 	plt.figure("Channel {} : {}".format(i, channel[i]))
+# 	plt.imshow(image1[:, :, i])
+# 	plt.colorbar()
 
 
+plt.show()
+
+rows, cols = 2, 2
+axes=[0]
+fig=plt.figure()
+
+for i in range(rows * cols):
+    # rand_img = np.random.randint(255, size=(200, 200))
+    axes.append(fig.add_subplot(rows, cols, i+1))
+    if i < 3:
+        subplot_title = "Channel {} : {}".format(i, channel[i])
+        axes[-1].set_title(subplot_title)
+        plt.imshow(image1[:, :, i])
+    else:
+        plt.imshow(image1)
+
+    plt.colorbar()
+    axes[-1].sharex(axes[-1])
+    axes[-1].sharey(axes[-1])
+
+fig.tight_layout()
 plt.show()
