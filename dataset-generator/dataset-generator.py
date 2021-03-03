87838f3a7ec4f8a51a07d38637df003e06494dee
Natsec <kamilp@hotmail.fr>
2021-03-03 10:40:12 +0100

    setting + space

--- dataset-generator/dataset-generator.py
+++ dataset-generator/dataset-generator.py
@@ -14,89 +14,89 @@ import subprocess
 # ---------------------------------------
 
 def get_random_text():
-	"""Gets and returns a random text to be hidden with Steganography.
+    """Gets and returns a random text to be hidden with Steganography.
 
-	Returns:
-	- **(str)**: A string that contains a random text comming from a local file.
-	"""
+    Returns:
+    - **(str)**: A string that contains a random text comming from a local file.
+    """
 
-	with open("dico.txt") as dico:
-		return random.choice(dico.readlines()).strip()
+    with open("dico.txt") as dico:
+        return random.choice(dico.readlines()).strip()
 
 # ---------------------------------------
 
 def set_text_file(temp_text_file, data_to_hide):
-	"""Creates a file and writes given text into it.
+    """Creates a file and writes given text into it.
 
-	Args:
-		temp_text_file (str): The temporary file to contain the secret text.
-		data_to_hide (str): The secret to write into the temporary file.
-	"""
+    Args:
+        temp_text_file (str): The temporary file to contain the secret text.
+        data_to_hide (str): The secret to write into the temporary file.
+    """
 
-	with open(temp_text_file, "w") as file:
-		file.write(data_to_hide)
+    with open(temp_text_file, "w") as file:
+        file.write(data_to_hide)
 
 # ---------------------------------------
 
 def stego_lsb(cover_image, text_file, stego_image):
-	"""Steganographies a random text within a cover image to create a stego image.
+    """Steganographies a random text within a cover image to create a stego image.
 
-	Args:
-		cover_image (str): The source image filename.
-		text_file (str): The filename containg the secret.
-		stego_image (str): The destination image filename.
-	"""
+    Args:
+        cover_image (str): The source image filename.
+        text_file (str): The filename containg the secret.
+        stego_image (str): The destination image filename.
+    """
 
-	try:
-		subprocess.call(["stegolsb", "steglsb", "-h", "-i", cover_image, "-s", text_file, "-o", stego_image])
-	except:
-		raise Exception("Steg LSB failed! Install the tool using [git clone https://github.com/ragibson/Steganography].\n")		
+    try:
+        subprocess.call(["stegolsb", "steglsb", "-h", "-i", cover_image, "-s", text_file, "-o", stego_image])
+    except:
+        raise Exception("Steg LSB failed! Install the tool using [git clone https://github.com/ragibson/Steganography].\n")
 
 # ---------------------------------------
 
 def main():
-	"""Runs the dataset generator program when executing the script from the command-line."""
+    """Runs the dataset generator program when executing the script from the command-line."""
 
-	# directories that contain cover images and future stego images
-	cover_images_dir = "./cover-images"
-	stego_images_dir = "./stego-images"
+    # directories that contain cover images and future stego images
+    cover_images_dir = "./cover-images"
+    stego_images_dir = "./stego-images"
 
-	# temporary file that will contain a random text to be hidden within an image
-	temp_text_file = "./tmp.txt"
+    # temporary file that will contain a random text to be hidden within an image
+    temp_text_file = "./tmp.txt"
 
-	try:
-		# files existance checks
-		if not os.path.isdir(cover_images_dir):
-			raise Exception("Cover images directory was not found!")
+    try:
+        # files existance checks
+        if not os.path.isdir(cover_images_dir):
+            raise Exception("Cover images directory was not found!")
 
-		if not os.path.isdir(stego_images_dir):
-			os.mkdir(stego_images_dir)
+        if not os.path.isdir(stego_images_dir):
+            os.mkdir(stego_images_dir)
 
-		# for each image do stego with random text
-		for cover_image in os.listdir(cover_images_dir):
-			stego_image = "{}/lsb_{}".format(stego_images_dir, cover_image)
-			cover_image = "{}/{}".format(cover_images_dir, cover_image)
+        # for each image do stego with random text
+        for cover_image in os.listdir(cover_images_dir):
+            stego_image = "{}/lsb_{}".format(stego_images_dir, cover_image)
+            cover_image = "{}/{}".format(cover_images_dir, cover_image)
 
-			if not os.path.isfile(cover_image):
-				raise Exception("[{}] was not found in cover imges directory!".format(cover_image))
+            if not os.path.isfile(cover_image):
+                raise Exception("[{}] was not found in cover imges directory!".format(cover_image))
 
-			# get a random string from file
-			data_to_hide = get_random_text()
+            # get a random string from file
+            data_to_hide = get_random_text()
 
-			# create a temporary file that will contain the random text
-			set_text_file(temp_text_file, data_to_hide)
+            # create a temporary file that will contain the random text
+            set_text_file(temp_text_file, data_to_hide)
 
-			# stego the cover image with the random text to create a new stego image
-			stego_lsb(cover_image, temp_text_file, stego_image)
-	except Exception as e:
-		print(e)
-	else:
-		print("\n***************\nProcess done!\n***************")
-	finally:
-		if os.path.isfile(temp_text_file):
-			os.remove(temp_text_file)
+            # stego the cover image with the random text to create a new stego image
+            stego_lsb(cover_image, temp_text_file, stego_image)
+    except Exception as e:
+        print(e)
+    else:
+        print("\n***************\nProcess done!\n***************")
+    finally:
+        if os.path.isfile(temp_text_file):
+            os.remove(temp_text_file)
 
 # ---------------------------------------
 
 if __name__ == '__main__':
-	main()
+    main()
